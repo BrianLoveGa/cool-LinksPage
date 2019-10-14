4920055c56be7d8a03ccb6971da102bfc1b3c35a
Brian Lovelewss <brian.loveless1@yahoo.com>
2019-10-14 01:08:11 -0400

    top third done

--- page.js
+++ page.js
@@ -1,3 +1,5 @@
-/// this is all just css and html - no javascript tricks here
-
 console.log("There is no Java used here ... except this in the console log");
+
+
+/// this is all just css and html - no javascript tricks here
+/// thanks to google, youtube, codepen, github, and css-tricks sites
