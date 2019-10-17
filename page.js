aca9c21a3a386b56fd449383f12c5140f914ae57
Brian Lovelewss <brian.loveless1@yahoo.com>
2019-10-17 10:41:06 -0400

    kk

--- page.js
+++ page.js
@@ -1,5 +1,4 @@
-console.log("There is no Java used here ... except this in the console log");
-
+console.log('There is no Java used here ... except this bit right here in the console log');
 
 /// this is all just css and html - no javascript tricks here
 /// thanks to google, youtube, codepen, github, and css-tricks sites
