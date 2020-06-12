4fde74a02db1b9cf9bdcbce563112fd016a41b34
Brian Loveless <Brian.loveless1@yahoo.com>
2020-06-12 10:30:41 -0400

    secure

--- page.js
+++ page.js
@@ -1,4 +1,4 @@
-console.log("There is no Java used here ... except this in the console log");
+console.log("There is no JavaScript used here ... except this in the console log");
 
 
 /// this is all just css and html - no javascript tricks here
